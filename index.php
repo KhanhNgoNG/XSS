99f860737ee56d63211177bfe9fb420aa4a77ba1
KhanhNgoNG <khanhlenguyenngo@gmail.com>
2025-04-13 19:01:48 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+echo "<h2>👋 Chào mừng đến với Demo XSS của bạn!</h2>";
+echo "<ul>";
+echo "<li><a href='sop_1/index.php'>Stored XSS - Page 1</a></li>";
+echo "<li><a href='sop_2/index.php'>Stored XSS - Page 2</a></li>";
+echo "<li><a href='xss_basic.php'>Basic XSS Example</a></li>";
+echo "</ul>";
+?>
